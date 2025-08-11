ec9b162fd62de84a2a32af4b5d8edebf2b5fe05c
harikiran713 <harikiranl713@gamil.com>
2025-08-11 10:59:44 +0530

    added missing security for the quiz page

--- quiz-hero/app/quiz/page.tsx
+++ quiz-hero/app/quiz/page.tsx
@@ -8,6 +8,15 @@ import QuizCreation from "../../components/quiz";
 export default async function Quiz()
 
 {
+    
+    const session= await getServerSession(authoptions)
+    console.log("hello")
+    console.log("SESSION:", session);
+
+    if(!session?.user)
+    {
+     return   redirect('/')
+    }
    
     return(
 <div className=" min-h-screen bg-gradient-to-br from-purple-100 via white to-pink-100 ">
